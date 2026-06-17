c1d938885a0e2ab174c909e19d9fb52c00ceb2a1
akscows-ui <akscows@gmail.com>
2026-06-16 23:01:16 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'alex-tracker-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event: Cache the core files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: Take control immediately
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+// Fetch event: Serve from cache if available, otherwise go to network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
